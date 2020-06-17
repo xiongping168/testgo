95a28a52ee1729144f076b657c7895262244d057
xiongping <xiongping@qq.com>
2020-06-17 12:09:52 +0800

    测试新增aaa  bbbb ccc

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ func main()  {
 	fmt.Println("测试Push，增加一行")
 	fmt.Println("哈哈哈....")
 	fmt.Println("测试pull")
+
+
+	fmt.Println("aaaaa")
+	fmt.Println("bbbbb")
+	fmt.Println("ccccc")
+	fmt.Println("ddddd")
 }
 
 
